fa88a98d33478e06e5c94f167b95ae40e533979c
Joshua <ojdaraakinsanya@gmail.com>
2025-08-13 21:59:13 +0100

    Navbar and Sidebar

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 ( () =>{
-    //JS Code
-})();
+
+//Sidebar
+
+const openSidebarButton = document.querySelector('#open-sidebar-btn');
+const closeSidebarButton = document.querySelector('#close-sidebar-btn');
+const sidebar = document.querySelector('#sidebar');
+
+openSidebarButton.addEventListener('click', function () {
+    sidebar.style.display = 'flex';
+})
+
+closeSidebarButton.addEventListener('click', function () {
+    sidebar.style.display = 'none';
+})
+
+})()
