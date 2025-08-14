ae7cd7383cec78130b2702096b5b5fad0cdb356d
Joshua <ojdaraakinsanya@gmail.com>
2025-08-14 12:16:47 +0100

    Improved sidebar

--- script.js
+++ script.js
@@ -5,13 +5,26 @@
 const openSidebarButton = document.querySelector('#open-sidebar-btn');
 const closeSidebarButton = document.querySelector('#close-sidebar-btn');
 const sidebar = document.querySelector('#sidebar');
+const sidebarBackground = document.querySelector('#sidebar-background');
+
+const CLOSED = 'closed';
 
 openSidebarButton.addEventListener('click', function () {
-    sidebar.style.display = 'flex';
+    sidebar.classList.remove(CLOSED);
+    sidebarBackground.classList.remove(CLOSED);
 })
 
-closeSidebarButton.addEventListener('click', function () {
-    sidebar.style.display = 'none';
-})
+closeSidebarButton.addEventListener('click', closeSidebarFunction);
+
+sidebarBackground.addEventListener('click', closeSidebarFunction);
+
+function closeSidebarFunction() {
+    if(!sidebar.classList.contains(CLOSED)) {
+        sidebar.classList.add(CLOSED);
+    }
+    if(!sidebarBackground.classList.contains(CLOSED)) {
+        sidebarBackground.classList.add(CLOSED);
+    }
+}
 
 })()
