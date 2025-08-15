74cba302e7df512c416a8eeeb4050b1e2be02ada
Joshua <ojdaraakinsanya@gmail.com>
2025-08-15 17:14:05 +0100

    Added Dark Mode

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 ( () =>{
 
-//Sidebar
+const bodyElement = document.body;
 
 const openSidebarButton = document.querySelector('#open-sidebar-btn');
 const closeSidebarButton = document.querySelector('#close-sidebar-btn');
@@ -8,6 +8,8 @@ const sidebar = document.querySelector('#sidebar');
 const sidebarBackground = document.querySelector('#sidebar-background');
 
 const CLOSED = 'closed';
+
+//Sidebar
 
 openSidebarButton.addEventListener('click', function () {
     sidebar.classList.remove(CLOSED);
@@ -25,6 +27,61 @@ function closeSidebarFunction() {
     if(!sidebarBackground.classList.contains(CLOSED)) {
         sidebarBackground.classList.add(CLOSED);
     }
+}
+
+// Theme toggle
+
+const themeToggleButton = document.querySelector('#theme-toggle-button');
+const themeTooltip = document.querySelector('#nav-tooltip');
+const lightIcon = document.querySelector('#light-icon');
+const darkIcon = document.querySelector('#dark-icon');
+
+const DARKMODE_CSS = 'dark';
+let isDark = false;
+
+lightIcon.style.display = 'inline';
+darkIcon.style.display = 'none';
+
+themeToggleButton.addEventListener('click', function () {
+    if(!isDark) {
+        if( !bodyElement.classList.contains(DARKMODE_CSS) ) {
+            bodyElement.classList.add(DARKMODE_CSS);
+        }
+        isDark = switchToDarkThemeUI(true);
+    }
+    else {
+        while (bodyElement.classList.contains(DARKMODE_CSS)) {
+            bodyElement.classList.remove(DARKMODE_CSS);
+        }
+        isDark = switchToDarkThemeUI(false);
+    }
+})
+
+themeToggleButton.addEventListener('mouseenter', function (){
+    themeTooltip.style.display = 'inline-block';
+})
+themeToggleButton.addEventListener('mouseleave', function () {
+    themeTooltip.style.display = 'none';
+})
+
+//false => Light; true => Dark
+function switchToDarkThemeUI(state=true) {
+    let darkState = false;
+    
+    //Dark theme
+    if(state) {
+        darkState = true;
+        lightIcon.style.display = 'none';
+        darkIcon.style.display = 'inline';
+    }
+    
+    //Light theme
+    if(!state) {
+        darkState = false;
+        darkIcon.style.display = 'none';
+        lightIcon.style.display = 'inline';
+    }
+    return darkState;
 }
 
 })()
